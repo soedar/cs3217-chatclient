32bb28b5a6b2f9290dec43af5d587d242a7279bb
soedar <soedar.sg@gmail.com>
2014-03-13 15:42:28 +0800

    Initial commit

--- ChatClient/ViewController.m
+++ ChatClient/ViewController.m
@@ -10,6 +10,13 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) IBOutlet UITableView *chatTableView;
+@property (nonatomic, weak) IBOutlet UITextField *messageTextField;
+@property (nonatomic, weak) IBOutlet UILabel *usernameLabel;
+
+@property (nonatomic, strong) NSMutableArray *messages;
+@property (nonatomic, strong) NSMutableArray *users;
+
 @end
 
 @implementation ViewController
@@ -18,12 +25,60 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.messages = [@[] mutableCopy];
+    self.users = [@[] mutableCopy];
+    
+    self.usernameLabel.text = [NSString stringWithFormat:@"User %i", arc4random()%10000];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Send Button Action
+
+- (IBAction)sendMessage:(UIButton *)sender
+{
+    // For now, just add the message to the list
+    [self addMessage:self.messageTextField.text fromUser:self.usernameLabel.text];
+    self.messageTextField.text = @"";
+}
+
+#pragma mark - Adding Message
+- (void)addMessage:(NSString *)message fromUser:(NSString *)user
+{
+    // Add a new message, and also update the tableview with the new message
+    [self.messages addObject:message];
+    [self.users addObject:user];
+    
+    NSIndexPath *newIndexPath = [NSIndexPath indexPathForRow:self.messages.count-1 inSection:0];
+    
+    [self.chatTableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationBottom];
+    [self.chatTableView scrollToRowAtIndexPath:newIndexPath atScrollPosition:UITableViewScrollPositionTop animated:YES];
+}
+
+#pragma mark - TableViewDelegate/DataSource
+
+// Probably don't need to edit anything below
+- (int)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.messages.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellId = @"Chat";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
+    }
+    
+    cell.textLabel.text = self.messages[indexPath.row];
+    cell.detailTextLabel.text = self.users[indexPath.row];
+    return cell;
 }
 
 @end
