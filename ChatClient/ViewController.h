1e7e00002248e156b7df27d6d6ae0b6e4db41d8e
soedar <soedar.sg@gmail.com>
2014-03-13 15:06:20 +0800

    Initial Commit

---
+++ ChatClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ChatClient
+//
+//  Created by soedar on 13/3/14.
+//  Copyright (c) 2014 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
